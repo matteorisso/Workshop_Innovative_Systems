17e6a7ccb58cb8649522b51e12a9d0c56d5f8da6
Matteo Risso <matteorisso96@gmail.com>
2019-05-21 21:05:37 +0200

    Aggiunte immagini e script
    - aggiunto script conversione png to immagine tipo MNIST
    - aggiunti numeri da 0 a 9 disegnati a mano per testare la rete con un dataset differente da MNIST

---
+++ SW_model/png_to_mnist.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 21 18:04:49 2019
+
+@author: matteorisso
+"""
+
+from PIL import Image, ImageFilter
+import numpy
+import matplotlib.pyplot as plt
+
+
+def imageprepare(argv):
+    """
+    This function returns the pixel values.
+    The imput is a png file location.
+    """
+    im = Image.open(argv).convert('L')
+    width = float(im.size[0])
+    height = float(im.size[1])
+    newImage = Image.new('L', (28, 28), (255))  # creates white canvas of 28x28 pixels
+
+    if width > height:  # check which dimension is bigger
+        # Width is bigger. Width becomes 20 pixels.
+        nheight = int(round((20.0 / width * height), 0))  # resize height according to ratio width
+        if (nheight == 0):  # rare case but minimum is 1 pixel
+            nheight = 1
+            # resize and sharpen
+        img = im.resize((20, nheight), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
+        wtop = int(round(((28 - nheight) / 2), 0))  # calculate horizontal position
+        newImage.paste(img, (4, wtop))  # paste resized image on white canvas
+    else:
+        # Height is bigger. Heigth becomes 20 pixels.
+        nwidth = int(round((20.0 / height * width), 0))  # resize width according to ratio height
+        if (nwidth == 0):  # rare case but minimum is 1 pixel
+            nwidth = 1
+            # resize and sharpen
+        img = im.resize((nwidth, 20), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
+        wleft = int(round(((28 - nwidth) / 2), 0))  # caculate vertical pozition
+        newImage.paste(img, (wleft, 4))  # paste resized image on white canvas
+
+    # newImage.save("sample.png
+
+    tv = list(newImage.getdata())  # get pixel values
+
+    # normalize pixels to 0 and 1. 0 is pure white, 1 is pure black.
+    tva = [(255 - x) * 1.0 / 255.0 for x in tv]
+    #print(tva)
+    return tva
+
+x=imageprepare('./immagini_test/test_LeNet5_9.png')#file path here
+img = numpy.array(x)
+img = img.reshape(28,28)
+plt.imshow(img, cmap='viridis', vmin=0, vmax=1)
