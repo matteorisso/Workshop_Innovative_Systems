a6d2e9e993c47b1c9add465469953ba2bba42ce3
Antonio Simone <antsimone03@gmail.com>
2019-06-21 20:41:21 +0200

    tb_conv processes

--- hw_model/conv/simulation/pe.vhd
+++ hw_model/conv/simulation/pe.vhd
@@ -1,6 +1,5 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all; 
 
 use work.fixed_pkg.all; 
 
@@ -13,11 +12,12 @@ port(
 		ld_v 			: in std_logic;
 		ld_h 			: in std_logic; 
 		sel			: in std_logic;
-		weight		: in  sfixed(qi-1 downto -qf)
+		weight		: in  sfixed(qi-1 downto -qf);
 		pe_right 	: in	sfixed(qi-1 downto -qf);
 		pe_bottom	: in	sfixed(qi-1 downto -qf);
+		pe_top		: out sfixed(qi-1 downto -qf);
 		pe_left		: out sfixed(qi-1 downto -qf);
-		pe_top		: out sfixed(qi-1 downto -qf));
+		omap			: out sfixed(qi-1 downto -qf));
 		
 end entity; 
 
@@ -56,16 +56,12 @@ port(
 end component; 
 
 
-signal d_reg 	: sfixed(qi-1 downto -qf);
-signal im_data	: sfixed(qi-1 downto -qf);
-signal d_acc	: sfixed(qi-1 downto -qf);
-signal q_acc	: sfixed(qi-1 downto -qf);
-
-
+signal d_reg		: sfixed(qi-1 downto -qf);
+signal q_reg 		: sfixed(qi-1 downto -qf);
 
 -- fractional scaling Q1.X
 constant nqi  : natural := 1; 
-constant nqf  : natural := psum'length-1; 
+constant nqf  : natural := pe_right'length-1; 
 
 signal mpy_a		: sfixed(nqi-1 downto -nqf) ;
 signal mpy_b 		: sfixed(nqi-1 downto -nqf) ; 		
@@ -83,20 +79,24 @@ signal q_acc    : sfixed( nqi+bitg -1 downto -nqf ) ;
 begin
 
 with sel select d_reg <= pe_right when '0', pe_bottom when others; 
-regh	: regn			generic map ( qi => nqi, qf => nqf ) port map (d_reg, ck, rstn, '1', im_data);
-regv	: regn			generic map ( qi => nqi, qf => nqf ) port map (d_reg, ck, rstn, sel, pe_top);
 
+regh	: regn			generic map ( qi => nqi, qf => nqf ) port map (d_reg, ck, rstn, ld_h, q_reg);
+regv	: regn			generic map ( qi => nqi, qf => nqf ) port map (d_reg, ck, rstn, ld_v, pe_top);
 
+mpy_a 	<= 	q_reg; 
+mpy_b 	<= 	weight; 
+mpy_res 	<=		mpy_tmp( mpy_tmp'high ) & 	
+					mpy_tmp( mpy_tmp'high downto - nqf ) ;
 
-mpy_a <= im_data;
-mpy_b <= weight; 
 
 mul 	: multiplier	generic map ( qi => nqi, 	   qf => nqf ) port map (mpy_a, mpy_b, mpy_tmp);
 add	: adder			generic map ( qi => nqi+bitg, qf => nqf ) port map (mpy_res, q_acc, d_acc);
 acc	: regn			generic map ( qi => nqi+bitg, qf => nqf ) port map (d_acc, ck, rstn, '1', q_acc);
 
 
-mpy_res 	<=		mpy_tmp( mpy_tmp'high ) & 	
-					mpy_tmp( mpy_tmp'high downto - nqf ) ;
 
+pe_left	<= q_reg; 
+omap 		<= q_acc(nqi+bitg-1 downto -(nqf-bitg)); -- chopping
+
+end architecture; 
 
