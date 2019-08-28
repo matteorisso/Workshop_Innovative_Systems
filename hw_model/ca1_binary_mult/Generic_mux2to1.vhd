e3782fe3877bacc8cce0580528fbb912a6812e8b
Matteo Risso <matteorisso96@gmail.com>
2019-08-28 15:30:09 +0200

    binary multiplier

--- hw_model/ca1_binary_mult/Generic_mux2to1.vhd
+++ hw_model/ca1_binary_mult/Generic_mux2to1.vhd
@@ -10,9 +10,10 @@ entity generic_mux2to1 is
 
 	generic(n : integer := 4);
 	port(
-		sel : in std_logic;
-		d_0, d_1 : in signed((n-1) downto 0);
-		d_out : out signed((n-1) downto 0)
+		sel 	: in std_logic;
+		d_0 	: in signed((n-1) downto 0);
+		d_1 	: in signed((n-1) downto 0);
+		d_out	: out signed((n-1) downto 0)
 	);
 
 end generic_mux2to1;
@@ -21,7 +22,8 @@ architecture behavior of generic_mux2to1 is
 
 begin
 
-	d_out <= d_0 when sel = '0' else
+	d_out <= d_0 when sel = '0' 
+			else
 				d_1;
 
 end behavior;
--- hw_model/ca1_binary_mult/binary_mult.vhd
+++ hw_model/ca1_binary_mult/binary_mult.vhd
@@ -15,29 +15,44 @@ entity binary_mult is
 	port(
 		act 	:	in	signed(n-1	downto	0);
 		weight	:	in 	std_logic;
-		d_out	: 	out signed(n	downto 	0)
+		d_out	: 	out signed(n-1	downto 	0)
 	);
 
 end binary_mult;
 
 architecture behavior of binary_mult is
 
-component generic_mux2to1 is
+	component generic_mux2to1 is
 
-	generic(
-		n	:	integer	:=	4
+		generic(
+			n	:	integer	:=	4
+			);
+		port(
+			sel 	: in 	std_logic;
+			d_0		: in 	signed((n-1) downto 0); 
+			d_1 	: in 	signed((n-1) downto 0);
+			d_out 	: out 	signed((n-1) downto 0)
 		);
-	port(
-		sel : in std_logic;
-		d_0, d_1 : in signed((n-1) downto 0);
-		d_out : out signed((n-1) downto 0)
-	);
 
-end
+	end component;
 
+	signal	act_ca1	:	signed(n-1	downto	0);
+	
 begin
 
-	d_out <= d_0 when sel = '0' else
-				d_1;
+	act_ca1 <= not act;
+	
+	mux : generic_mux2to1
+		generic map(
+			n => 4
+		)
+		port map(
+			sel		=>	weight,
+			d_0		=>	act_ca1,
+			d_1 	=>	act,
+			d_out	=>	d_out
+		);
+					
+	
 
 end behavior;
